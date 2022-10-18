fa4115bb0066c2c05dfc8d18c8eea24b692b3475
Pavel Degtiarev <pavel.degtiarev@gmail.com>
2022-10-18 22:15:20 +0700

    мелкий рефакторинг

--- src/components/container/container.jsx
+++ src/components/container/container.jsx
@@ -1,6 +1,13 @@
 import React from "react";
+import PropTypes from "prop-types";
 import styles from "./container.module.scss";
 
 export function Container({ children }) {
+  console.log("render Container");
+
   return <div className={styles.container}>{children}</div>;
-}
+};
+
+Container.propTypes = {
+  children: PropTypes.node,
+};
--- src/components/search/search.jsx
+++ src/components/search/search.jsx
@@ -1,15 +1,17 @@
-import React from "react";
+import React, { memo } from "react";
 import PropTypes from "prop-types";
 import styles from "./search.field.module.scss";
 
-export function Search({ clickHandler }) {
+export const Search = memo(function Search({ clickHandler }) {
+  console.log("render Search");
+
   return (
     <div className={styles.search}>
       <input className={styles.searchField} type="text" name="searchField" id="searchField" />
       <button className={styles.searchButton} onClick={clickHandler}></button>
     </div>
   );
-}
+});
 
 Search.propTypes = {
   clickHandler: PropTypes.func,
