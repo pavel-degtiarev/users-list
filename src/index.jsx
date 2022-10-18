8724a3120465fa7b1feacbc97a0acec11a2baa67
Pavel Degtiarev <pavel.degtiarev@gmail.com>
2022-10-18 13:48:51 +0700

    добавлена точка входа для react

---
+++ src/index.jsx
@@ -0,0 +1,9 @@
+"use strict";
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+// import "./index.scss";
+
+const root = ReactDOM.createRoot(document.getElementById("main"));
+root.render(<h1>Привет, мир!</h1>);
