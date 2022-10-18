206e79ad9b99fe439e11ab0a38fd3facddaec25d
Pavel Degtiarev <pavel.degtiarev@gmail.com>
2022-10-18 22:09:19 +0700

    в Card передается объект user целиком

--- src/components/card.list/card.list.jsx
+++ src/components/card.list/card.list.jsx
@@ -1,23 +1,17 @@
-import React from "react";
+import React, { memo } from "react";
 import PropTypes from "prop-types";
 import styles from "./cards.list.module.scss";
 import { Card } from "../card/card";
 
-export function CardsList({ cards, cardClickHandler }) {
+export const CardsList = memo(function CardsList({ cards, cardClickHandler }) {
   return (
     <ul className={styles.cardsList}>
-      {cards.map((card, index) => (
-        <Card
-          key={card.name}
-          title={card.name}
-          phone={card.phone}
-          email={card.email}
-          clickHandler={cardClickHandler}
-        />
+      {cards.map((card) => (
+        <Card key={card.name} userData={card} clickHandler={cardClickHandler} />
       ))}
     </ul>
   );
-}
+})
 
 CardsList.propTypes = {
   cards: PropTypes.array,
